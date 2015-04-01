620a5f4c7955732102ab391782db12c5f82a7c1d
sanemat <o.gata.ken@gmail.com>
2015-04-01 22:52:39 +0900

    chore(power-assert): use power assert

--- test/close-your-issues_test.js
+++ test/close-your-issues_test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var closeYourIssues = require('../app/js/close-your-issues');
-var assert = require('assert');
+var assert = require('power-assert');
 
 describe('closeYourIssues', function () {
 
