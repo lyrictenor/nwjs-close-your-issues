bcec8ffda00b9580dbb75f0dc93e73ec7cd6b13c
sanemat <o.gata.ken@gmail.com>
2015-04-01 22:48:16 +0900

    chore(assert): use assert

--- test/close-your-issues_test.js
+++ test/close-your-issues_test.js
@@ -1,12 +1,12 @@
 'use strict';
 
 var closeYourIssues = require('../app/js/close-your-issues');
-var assert = require('should');
+var assert = require('assert');
 
 describe('closeYourIssues', function () {
 
   it('should be awesome', function () {
-    closeYourIssues().should.equal('awesome');
+    assert.equal(closeYourIssues(), 'awesome');
   });
 
 });
