03ed997f0f8e79a45c793b2750ae68ba568190bd
sanemat <o.gata.ken@gmail.com>
2015-04-03 00:29:47 +0900

    chore(flux): use material-flux and react

--- app/js/app.js
+++ app/js/app.js
@@ -1,2 +1,12 @@
 'use strict';
-require('./close-your-issues');
+import React from 'react';
+import Context from './Context';
+import App from './components/App.jsx';
+
+let context = new Context();
+let action = context.appAction;
+
+React.render(
+  React.createElement(App, { context }),
+  document.getElementById('close-your-issues-app')
+);
