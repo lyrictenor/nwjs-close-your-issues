e5dceea59adbb6cddbb09c26fd1894ca4622afb9
sanemat <o.gata.ken@gmail.com>
2015-04-03 19:44:02 +0900

    chore(github): use octonode

--- app/js/actions/Actions.js
+++ app/js/actions/Actions.js
@@ -1,5 +1,32 @@
 import { Action } from 'material-flux';
+import Github from 'octonode';
+var fs = require('fs');
+
 export var keys = {
 };
+
+function getCurrentIssues() {
+  return new Promise((resolve, reject) => {
+    let issues = JSON.parse(fs.readFileSync(__dirname + '/../../issues.json', 'utf8'));
+    resolve(issues);
+  });
+}
+
 export default class AppAction extends Action {
+
+  getIssues() {
+    //let client = Github.client();
+    //client
+    //  .repo('sanemat/nwjs-close-your-issues')
+    //  .issues({
+    //    page: 1,
+    //    per_page: 100,
+    //    state: 'open'
+    //  },
+    //  (err, body, header) => {
+    //    body;
+    //  });
+    getCurrentIssues().then((issues) => {
+    });
+  }
 }
