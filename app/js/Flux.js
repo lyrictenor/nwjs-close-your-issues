030a1d8b1c2f98349e4eba7e612d5af5636cdf49
sanemat <o.gata.ken@gmail.com>
2015-04-13 16:55:08 +0900

    chore(immutable): use immutable

--- app/js/Flux.js
+++ app/js/Flux.js
@@ -1,13 +1,13 @@
 'use strict';
 import { Flummox } from 'flummox';
 import IssueActions from './actions/IssueActions';
-//import DocStore from './stores/DocStore';
+import IssueStore from './stores/IssueStore';
 
 export default class Flux extends Flummox {
   constructor() {
     super();
 
     const issueActions = this.createActions('issues', IssueActions);
-    //this.createStore('docs', DocStore, { docActions });
+    this.createStore('issues', IssueStore, { issueActions });
   }
 }
---
+++ app/js/stores/IssueStore.js
@@ -0,0 +1,24 @@
+import { Store } from 'flummox';
+import { Map } from 'immutable';
+
+export default class IssueStore extends Store {
+  constructor({ issueActions }) {
+    super();
+
+    this.register(issueActions.getIssues, this.handleIssues);
+
+    this.state = {
+      issues: new Map()
+    };
+  }
+  handleIssues(allIssues) {
+    const issues = allIssues.reduce((result, issue) => {
+      result[issue.id] = issue;
+      return result;
+    }, {});
+
+    this.setState({
+      issues: this.state.issues.merge(issues)
+    });
+  }
+}
