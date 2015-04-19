43694dacce8402de3345adcabaa5dc8112d83075
sanemat <o.gata.ken@gmail.com>
2015-04-19 22:41:31 +0900

    chore(app): copy from IdiomaticReact

--- app/js/components/AppHandler.jsx
+++ app/js/components/AppHandler.jsx
@@ -1,21 +1,27 @@
 'use strict';
 import React from 'react';
 import { RouteHandler, Link } from 'react-router';
+import FluxComponent from 'flummox/component';
 
 class AppHandler extends React.Component {
+  componentDidMount() {
+    //fetch issues
+  }
 
   render() {
     return (
-      <div>
-        <header>
-          Title here...
-          <ul>
-            <li><Link to="issues">Issues</Link></li>
-            <li><Link to="settings">Settings</Link></li>
-          </ul>
-        </header>
-          {/* this is the important part */}
-        <RouteHandler/>
+      <div className='main container'>
+        <FluxComponent {...this.props} connectToStores={['issues']}>
+          <header>
+            Title here...
+            <ul>
+              <li><Link to="issues">Issues</Link></li>
+              <li><Link to="settings">Settings</Link></li>
+            </ul>
+          </header>
+            {/* this is the important part */}
+          <RouteHandler/>
+        </FluxComponent>
       </div>
     );
   }
