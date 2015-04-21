1628fcf7f1bd5f5721499daf6642ca860aeb8bcb
sanemat <o.gata.ken@gmail.com>
2015-04-21 17:35:45 +0900

    chore(nwjs): build application

--- bin/build.sh
+++ bin/build.sh
@@ -4,7 +4,7 @@ BUILD_APP_NAME="close-your-issues"
 TEST_PATH=$(pwd)
 
 if [[ "${TRAVIS_TAG}" ]]; then
-  npm run dist
+  grunt build
   $(npm bin)/rimraf "${TEST_PATH}/build"
   $(npm bin)/nwbuild -p 'win32,win64,osx32,osx64,linux32,linux64' "${TEST_PATH}/dist" -o "${TEST_PATH}/build"
   BUILD_TARGETS=(osx32 osx64 win32 win64 linux32 linux64)
