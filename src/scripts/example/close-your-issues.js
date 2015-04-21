1fa99bd5e8dc4a43bf07e6eb487bdc72e6e9580d
sanemat <o.gata.ken@gmail.com>
2015-04-21 17:51:40 +0900

    chore(test): copy from original

---
+++ src/scripts/example/close-your-issues.js
@@ -0,0 +1,13 @@
+/*
+ * close-your-issues
+ * http://sane.jp
+ *
+ * Copyright (c) 2015 sanemat
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = () => {
+  return 'awesome';
+};
---
+++ test/close-your-issues_test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import closeYourIssues from '../src/scripts/example/close-your-issues';
+import assert from 'power-assert';
+
+describe('closeYourIssues', () => {
+
+  it('should be awesome', () => {
+    assert.equal(closeYourIssues(), 'awesome');
+  });
+
+});
