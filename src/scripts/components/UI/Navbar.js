5b03d7b649867d3bf69122e2463f34e917bbd35a
sanemat <o.gata.ken@gmail.com>
2015-04-23 02:17:21 +0900

    chore(nav): fix title

--- src/scripts/components/UI/Navbar.js
+++ src/scripts/components/UI/Navbar.js
@@ -15,11 +15,11 @@ let UINavbar = React.createClass({
             <div className="navbar navbar-default navbar-fixed-top" role="navigation">
                 <div className="container">
                     <div className="navbar-header">
-                        <Link to="home" className="navbar-brand"><img src="assets/images/netgusto.png" style={{height: '60px', position: 'absolute', top: '5px'}} /> <div style={{marginLeft: '80px', fontSize: '1.3em'}}>Idiomatic React</div></Link>
+                        <Link to="home" className="navbar-brand"><img src="assets/images/netgusto.png" style={{height: '60px', position: 'absolute', top: '5px'}} /> <div style={{marginLeft: '80px', fontSize: '1.3em'}}>Close Your Issues</div></Link>
                     </div>
 
                     <ul className="nav navbar-nav">
-                        <li className={cx({active: this.isActive('rest')})}><Link to="rest">Rest Issues</Link></li>
+                        <li className={cx({active: this.isActive('rest')})}><Link to="rest">Issues</Link></li>
                     </ul>
 
                     <ul className="nav navbar-nav pull-right">
