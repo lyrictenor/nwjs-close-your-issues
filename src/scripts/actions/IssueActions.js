5dad48d7fda0d999725e8a203df805b4e1cd9d32
sanemat <o.gata.ken@gmail.com>
2015-04-22 20:48:14 +0900

    chore(issue): remove axios, use issue

--- src/scripts/actions/IssueActions.js
+++ src/scripts/actions/IssueActions.js
@@ -1,24 +1,23 @@
 'use strict';
 
 import { Actions } from 'flummox';
-import axios from 'axios';
 import uuid from '../utils/uuid';
 
 let serverFetchIssues = async function(apiendpoint) {
-    let issues = await axios.get(apiendpoint + '/todos');
-    return issues.data.slice(0, 7);  // passed to the store after REST response (obviously); sliced for the demo
+    let issues = await require('../../issues.json');
+    return issues;
 };
 
 let serverCreateIssue = function(apiendpoint, issueContent) {
 
     const newIssue = { id: uuid(), title: issueContent };
-    axios.post(apiendpoint + '/todos', newIssue);
+    //axios.post(apiendpoint + '/todos', newIssue);
 
     return newIssue; // passed to the store without awaiting REST response for optimistic add
 };
 
 let serverDeleteIssue = function(apiendpoint, issue) {
-    axios.delete(apiendpoint + '/todos/' + issue.get('id'));
+    //axios.delete(apiendpoint + '/todos/' + issue.get('id'));
     return issue; // passed to the store without awaiting REST response for optimistic delete
 };
 
