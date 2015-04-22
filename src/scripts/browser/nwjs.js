878d56509c306e86b2a9491cb0ee017da89f9afc
sanemat <o.gata.ken@gmail.com>
2015-04-22 14:34:45 +0900

    chore(nwjs): simplify menu option

--- src/scripts/browser/nwjs.js
+++ src/scripts/browser/nwjs.js
@@ -3,9 +3,7 @@
   if (typeof process === 'object' && process.platform === 'darwin') {
     var gui = require('nw.gui');
     var mb = new gui.Menu({type: 'menubar'});
-    mb.createMacBuiltin('CloseYourIssues', {
-      hideEdit: false
-    });
+    mb.createMacBuiltin('CloseYourIssues');
     gui.Window.get().menu = mb;
   }
 
