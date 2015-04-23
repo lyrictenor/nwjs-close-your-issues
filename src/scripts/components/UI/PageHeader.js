2ad9a07a9900a3f3a8a4e660197b57b2eededddc
sanemat <o.gata.ken@gmail.com>
2015-04-23 18:04:17 +0900

    style(nav): fix indent

--- src/scripts/components/UI/PageHeader.js
+++ src/scripts/components/UI/PageHeader.js
@@ -3,14 +3,14 @@
 import React from 'react/addons';
 
 let UIPageHeader = React.createClass({
-    render() {
-        return (
-            <div>
-                <h2 className="pageheader"><i className={'fa fa-' + this.props.icon }></i> {this.props.text}</h2>
-                <hr />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h2 className="pageheader"><i className={'fa fa-' + this.props.icon }></i> {this.props.text}</h2>
+        <hr />
+      </div>
+    );
+  }
 });
 
 module.exports = UIPageHeader;
