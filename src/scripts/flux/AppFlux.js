8b0b9d9e21d39e968a806e1d42cbd21a7344a046
sanemat <o.gata.ken@gmail.com>
2015-04-24 20:11:49 +0900

    style(flux): fix indent

--- src/scripts/flux/AppFlux.js
+++ src/scripts/flux/AppFlux.js
@@ -8,18 +8,21 @@ import { IssueStore } from '../stores/IssueStore';
 
 export class AppFlux extends Flux {
 
-    constructor(config) {
-        super();
+  constructor(config) {
+    super();
 
-        this.config = Immutable.fromJS(config);
+    this.config = Immutable.fromJS(config);
 
-         // The extra argument(s) are passed to the Action / Store constructors
-        this.createActions('issues', IssueActions, this);
-        this.createStore('issues', IssueStore, this);
-    }
+    // The extra argument(s) are passed to the Action / Store constructors
+    this.createActions('issues', IssueActions, this);
+    this.createStore('issues', IssueStore, this);
+  }
 
-    getApiendpoint() { return this.config.get('apiendpoint'); }
+  getApiendpoint() {
+    return this.config.get('apiendpoint');
+  }
 
-    getUser() { return this.config.get('user').toJS(); }
-
+  getUser() {
+    return this.config.get('user').toJS();
+  }
 }
