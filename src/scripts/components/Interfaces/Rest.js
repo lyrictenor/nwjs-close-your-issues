05b67fdad1107ead70c8806207ead0d5288c41bf
sanemat <o.gata.ken@gmail.com>
2015-04-28 03:32:43 +0900

    chore(issue): remove form

--- src/scripts/components/Interfaces/Rest.js
+++ src/scripts/components/Interfaces/Rest.js
@@ -3,21 +3,14 @@
 import React from 'react/addons';
 import UIPageHeader from '../UI/PageHeader';
 import IssueList from '../Issue/IssueList';
-import IssueForm from '../Issue/IssueForm';
 
 class InterfaceRest extends React.Component {
   render() {
-
-    const handleAdd = (title) => {
-      this.props.flux.getActions('issues').createIssue(title);
-    };
-
     return (
       <div>
         <UIPageHeader icon="gear" text='Issues' />
 
         <IssueList {...this.props} />
-        <IssueForm {...this.props} onAdd={handleAdd} />
       </div>
     );
   }
