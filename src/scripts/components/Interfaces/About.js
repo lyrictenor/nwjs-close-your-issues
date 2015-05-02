7f9737606bd1b933f2ef26a6907b2903ec75499a
sanemat <o.gata.ken@gmail.com>
2015-05-02 09:12:10 +0900

    style(about): fix indent

--- src/scripts/components/Interfaces/About.js
+++ src/scripts/components/Interfaces/About.js
@@ -4,26 +4,26 @@ import React from 'react/addons';
 import UIPageHeader from 'components/UI/PageHeader';
 
 class InterfaceAbout extends React.Component {
-    render() {
+  render() {
 
-        //const user = this.props.flux.getUser();
+    //const user = this.props.flux.getUser();
 
-        return (
-            <div>
-                <UIPageHeader icon="gear" text={'you, welcome !'} />
-                <p><strong>Close Your Issues</strong> aims to be the simplest API-centric Flux app one could build using React.</p>
-                <p>Currently, it uses :</p>
-                <ul>
-                    <li><a href="http://facebook.github.io/react/">React</a> with <a href="https://facebook.github.io/jsx/">JSX</a>;</li>
-                    <li><a href="https://github.com/acdlite/flummox">Flummox</a> (<a href="http://facebook.github.io/flux/">Flux implementation</a>) for the stores;</li>
-                    <li><a href="https://github.com/rackt/react-router">React-Router</a>;</li>
-                    <li><a href="http://facebook.github.io/immutable-js/">Immutable.js</a> for immutability in the stores;</li>
-                    <li><a href="https://babeljs.io/">Babel</a> for ES6/ES7 transpilation and linting;</li>
-                    <li><a href="http://webpack.github.io/">Webpack</a> for the tooling.</li>
-                </ul>
-            </div>
-        );
-    }
+    return (
+      <div>
+        <UIPageHeader icon="gear" text={'you, welcome !'} />
+        <p><strong>Close Your Issues</strong> aims to be the simplest API-centric Flux app one could build using React.</p>
+        <p>Currently, it uses :</p>
+        <ul>
+          <li><a href="http://facebook.github.io/react/">React</a> with <a href="https://facebook.github.io/jsx/">JSX</a>;</li>
+          <li><a href="https://github.com/acdlite/flummox">Flummox</a> (<a href="http://facebook.github.io/flux/">Flux implementation</a>) for the stores;</li>
+          <li><a href="https://github.com/rackt/react-router">React-Router</a>;</li>
+          <li><a href="http://facebook.github.io/immutable-js/">Immutable.js</a> for immutability in the stores;</li>
+          <li><a href="https://babeljs.io/">Babel</a> for ES6/ES7 transpilation and linting;</li>
+          <li><a href="http://webpack.github.io/">Webpack</a> for the tooling.</li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default InterfaceAbout;
