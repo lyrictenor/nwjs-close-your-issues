f74427cd3881dcb7f1825e7550717dd89f393f2a
sanemat <o.gata.ken@gmail.com>
2015-05-02 13:23:29 +0900

    chore(issue): extract issue to issue card

--- src/scripts/components/Issue/IssueList.js
+++ src/scripts/components/Issue/IssueList.js
@@ -1,59 +1,12 @@
 'use strict';
 
 import React from 'react/addons';
-import GithubUrl from 'github-url-to-object';
-import onOpenExternals from 'myUtils/openExternals';
+import IssueCard from 'components/Issue/IssueCard';
 
-class IssueList extends React.Component {
-  //TODO move this to business logic
-  getSlug(repo) {
-    let GithubObject = GithubUrl(repo);
-    return `${GithubObject.user}/${GithubObject.repo}`;
-  }
-  //TODO move this to business logic
-  trimWidth(string, length=100) {
-    return `${string.slice(0, length)}...`;
-  }
-
+export default class IssueList extends React.Component {
   render() {
-    const onDelete = (issue) => {
-      this.props.flux.getActions('issues').deleteIssue(issue);
-    };
-
     const issues = this.props.issues.map(issue =>
-        <li>
-          <div>
-            <span className="octicon octicon-issue-opened"></span>
-            <a href={issue.html_url} onClick={onOpenExternals.bind(this)}>
-              {issue.title}
-            </a>
-          </div>
-          <div>
-            #{issue.number}
-            opened {issue.created_at}
-            by <img src={issue.user.avatar_url} style={{width: '20px', height: '20px'}} /> {issue.user.login}
-          </div>
-          <div>
-            {this.getSlug(issue.html_url)}
-          </div>
-          <div>
-            {this.trimWidth(issue.body_text)}
-          </div>
-          <div>
-            <button className="btn btn-default btn-sm" onClick={onDelete.bind(this, issue)}>
-              Delete Branch
-            </button>
-            <button className="btn btn-default btn-sm" onClick={onDelete.bind(this, issue)}>
-              Close Issue
-            </button>
-            <button className="btn btn-default btn-sm" onClick={onDelete.bind(this, issue)}>
-              Merge Pull Request
-            </button>
-            <button className="btn btn-default btn-sm" onClick={onDelete.bind(this, issue)}>
-              Snooze
-            </button>
-          </div>
-        </li>
+      <IssueCard {...this.props} issue={issue} />
     );
 
     return (
@@ -66,7 +19,4 @@ class IssueList extends React.Component {
       </div>
     );
   }
-
 }
-
-export default IssueList;
