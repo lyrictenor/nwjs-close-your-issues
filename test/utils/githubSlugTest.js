91951cafb6c4cb1c7382cbcfd0ddd2f53da46d02
sanemat <o.gata.ken@gmail.com>
2015-05-04 23:03:41 +0900

    style(utils): use double quote

--- test/utils/githubSlugTest.js
+++ test/utils/githubSlugTest.js
@@ -1,11 +1,11 @@
-'use strict';
+"use strict";
 
-import githubSlug from '../../src/scripts/utils/githubSlug';
-import assert from 'power-assert';
+import githubSlug from "../../src/scripts/utils/githubSlug";
+import assert from "power-assert";
 
-describe('githubSlug', () => {
-  it('detects slug', () => {
-    const repoUrl = 'https://github.com/lyrictenor/nwjs-close-your-issues';
-    assert.equal(githubSlug(repoUrl), 'lyrictenor/nwjs-close-your-issues');
+describe("githubSlug", () => {
+  it("detects slug", () => {
+    const repoUrl = "https://github.com/lyrictenor/nwjs-close-your-issues";
+    assert.equal(githubSlug(repoUrl), "lyrictenor/nwjs-close-your-issues");
   });
 });
