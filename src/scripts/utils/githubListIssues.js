fdc0cc9b168d7960cf6e6e14718da154928f7aaa
sanemat <o.gata.ken@gmail.com>
2015-05-08 16:08:57 +0900

    chore(issue): fix comment

--- src/scripts/utils/githubListIssues.js
+++ src/scripts/utils/githubListIssues.js
@@ -1,7 +1,7 @@
 "use strict";
 import axios from "axios";
 
-// https://developer.github.com/v3/issues/githubListIssues.js#list-issues
+// https://developer.github.com/v3/issues/#list-issues
 // GET /issues
 module.exports = async (url, config) => {
   return await axios.get(url, config);
