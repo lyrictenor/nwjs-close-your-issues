a63518fd2a2f7d4b0c1eafd35ef442e903f2b96e
sanemat <o.gata.ken@gmail.com>
2015-05-08 16:06:42 +0900

    docs(issue): fix comment

--- src/scripts/utils/githubGetSingleRepository.js
+++ src/scripts/utils/githubGetSingleRepository.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 // https://developer.github.com/v3/repos/#get
-// GET GET /repos/:owner/:repo
+// GET /repos/:owner/:repo
 module.exports = async (repoUrl, config) => {
   return await axios.get(repoUrl, config);
 };
