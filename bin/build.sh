3fb77ac2e77f0269ee306cce6041f9ebccfb17df
sanemat <o.gata.ken@gmail.com>
2015-05-12 23:07:17 +0900

    chore(app): add node-webkit target version

--- bin/build.sh
+++ bin/build.sh
@@ -6,7 +6,8 @@ TEST_PATH=$(pwd)
 if [[ "${TRAVIS_TAG}" ]]; then
   npm run build
   $(npm bin)/rimraf "${TEST_PATH}/build"
-  $(npm bin)/nwbuild -p 'win32,win64,osx32,osx64,linux32,linux64' "${TEST_PATH}/dist" -o "${TEST_PATH}/build"
+  # FIXME: update target to 0.13
+  $(npm bin)/nwbuild -v "0.12.1" -p 'win32,win64,osx32,osx64,linux32,linux64' "${TEST_PATH}/dist" -o "${TEST_PATH}/build"
   BUILD_TARGETS=(osx32 osx64 win32 win64 linux32 linux64)
   for BUILD_TARGET in ${BUILD_TARGETS[@]}; do
     cd "${TEST_PATH}/build/${BUILD_APP_NAME}/${BUILD_TARGET}"
