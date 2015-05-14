efe75bb842f406cee07479b08e29ec8ffc14faba
sanemat <o.gata.ken@gmail.com>
2015-05-14 17:32:47 +0900

    chore(issue): add relation

--- src/scripts/utils/persistence.js
+++ src/scripts/utils/persistence.js
@@ -77,12 +77,86 @@ export const saveUsersAndRepositories = async (repositories) => {
 export const saveIssues = async (issues) => {
   let db = await dbConnection();
 
+  // set up users from issue's repository's owner, issue's user, issue's assignee
+  let usersTable = await db.getSchema().table("Users");
+  let userRows = issues.reduce((previous, current) => {
+    // issue's repository's owner
+    /* eslint-disable camelcase */
+    let userParams = Object.assign({}, current.repository.owner);
+    userParams.created_at = (userParams.created_at) ? new Date(userParams.created_at) : null;
+    userParams.updated_at = (userParams.updated_at) ? new Date(userParams.updated_at) : null;
+    /* eslint-eable camelcase */
+
+    previous.push(
+      usersTable.createRow(userParams)
+    );
+
+    // issue's user
+    /* eslint-disable camelcase */
+    userParams = Object.assign({}, current.user);
+    userParams.created_at = (userParams.created_at) ? new Date(userParams.created_at) : null;
+    userParams.updated_at = (userParams.updated_at) ? new Date(userParams.updated_at) : null;
+    /* eslint-eable camelcase */
+
+    previous.push(
+      usersTable.createRow(userParams)
+    );
+
+    // issue's assignee
+    if (current.assignee) {
+      /* eslint-disable camelcase */
+      userParams = Object.assign({}, current.assignee);
+      userParams.created_at = (userParams.created_at) ? new Date(userParams.created_at) : null;
+      userParams.updated_at = (userParams.updated_at) ? new Date(userParams.updated_at) : null;
+      /* eslint-eable camelcase */
+
+      previous.push(
+        usersTable.createRow(userParams)
+      );
+    }
+
+    return previous;
+  }, []);
+
+  // insert_or_replace users
+  await db.insertOrReplace().into(usersTable).values(userRows).exec();
+
+  // set up repositories from issues
+  let repositoriesTable = await db.getSchema().table("Repositories");
+  let repositoryRows = issues.reduce((previous, current) => {
+    /* eslint-disable camelcase */
+    let repositoryParams = Object.assign({}, current.repository);
+    let owner = Object.assign({}, current.owner);
+    delete repositoryParams.owner;
+    repositoryParams.owner = owner.id;
+    repositoryParams.created_at = new Date(current.created_at);
+    repositoryParams.updated_at = new Date(current.updated_at);
+    repositoryParams.pushed_at = (current.pushed_at) ? new Date(current.pushed_at) : null;
+    /* eslint-eable camelcase */
+
+    previous.push(
+      repositoriesTable.createRow(repositoryParams)
+    );
+    return previous;
+  }, []);
+
+  // insert_or_replace repositories
+  await db.insertOrReplace().into(repositoriesTable).values(repositoryRows).exec();
+
   // set up issues
   let issuesTable = await db.getSchema().table("Issues");
   let issueRows = issues.reduce((previous, current) => {
     /* eslint-disable camelcase */
-    // TODO: if (!repository || !user || !assignee) { create record; }
     let issueParams = Object.assign({}, current);
+    let assignee = Object.assign({}, current.assignee);
+    let user = Object.assign({}, current.user);
+    let repository = Object.assign({}, current.repository);
+    delete issueParams.assignee;
+    delete issueParams.user;
+    delete issueParams.repository;
+    issueParams.assignee = assignee.id;
+    issueParams.user = user.id;
+    issueParams.repository = repository.id;
     issueParams.created_at = new Date(current.created_at);
     issueParams.updated_at = new Date(current.updated_at);
     issueParams.closed_at = (current.closed_at) ? new Date(current.closed_at) : null;
