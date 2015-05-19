a3c6af2ffbb1d29d1757060e2f176a3f0711ee18
sanemat <o.gata.ken@gmail.com>
2015-05-19 14:42:55 +0900

    chore(crypto): spike

--- src/scripts/utils/githubDefaultConfig.js
+++ src/scripts/utils/githubDefaultConfig.js
@@ -1,11 +1,15 @@
 "use strict";
+import AES from "crypto-js/aes";
+import cryptoJsEncUtf8 from "crypto-js/enc-utf8";
 
 module.exports = (token) => {
   if (token) {
+    const decrypted = AES.decrypt(token, "Thohh3quohgh0u");
+    console.log(decrypted);
     return {
       headers: {
         Accept: "application/vnd.github.moondragon.text+json",
-        Authorization: `token ${token}`
+        Authorization: `token ${decrypted.toString(cryptoJsEncUtf8)}`
       }
     };
   } else {
--- src/scripts/utils/persistence.js
+++ src/scripts/utils/persistence.js
@@ -2,6 +2,7 @@
 
 import removeTrailingSlash from "myUtils/removeTrailingSlash";
 import githubSlug from "myUtils/githubSlug";
+import AES from "crypto-js/aes";
 
 export const initConfig = async () => {
   const savedParams = dataToParams(await getPersistedConfigData());
@@ -17,12 +18,21 @@ export const initConfig = async () => {
   return params;
 };
 
+const encryptToken = (token) => {
+  if (!token) {
+    return token;
+  }
+  const encrypted = AES.encrypt(token, "Thohh3quohgh0u");
+  console.log(encrypted);
+  return encrypted.toString();
+};
+
 export const saveConfig = async (settings) => {
   // mapping
   let params = {};
   params.apiendpoint = removeTrailingSlash(settings.apiendpoint);
   params.webendpoint = removeTrailingSlash(settings.webendpoint);
-  params.token = settings.token;
+  params.token = encryptToken(settings.token);
   params.slug = removeTrailingSlash(settings.slug);
 
   await persistConfigParams(params);
