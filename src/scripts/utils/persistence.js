c1491bccc8a042f29d0665ee4c26c438c01f5f1a
sanemat <o.gata.ken@gmail.com>
2015-05-22 15:57:23 +0900

    chore(pulls): add users

--- src/scripts/utils/persistence.js
+++ src/scripts/utils/persistence.js
@@ -307,6 +307,96 @@ const currentRepository = (data, htmlUrl) => {
 export const savePullRequests = async (pulls) => {
   let db = await dbConnection();
   let lf = window.lf;
+  const ids = pulls.map((pull) => {
+    return pull.id;
+  });
+  // set up users from pull-head-repo's owner, pull-head's user, pull's assignee, pull's user,
+  // pull-base-repo's owner, pull-base's user
+  let usersTable = await db.getSchema().table("Users");
+  let userRows = pulls.reduce((previous, current) => {
+    let userParams;
+    // pull-head-repo's owner
+    if (current.head && current.haed.repo && current.head.repo.owner && !doesRowsIncludesId(previous, current.head.repo.owner.id)) {
+      /* eslint-disable camelcase */
+      userParams = Object.assign({}, current.head.repo.owner);
+      userParams.created_at = (userParams.created_at) ? new Date(userParams.created_at) : null;
+      userParams.updated_at = (userParams.updated_at) ? new Date(userParams.updated_at) : null;
+      /* eslint-eable camelcase */
+
+      previous.push(
+        usersTable.createRow(userParams)
+      );
+    }
+
+    // pull-head's user
+    if (current.head && current.head.user && !doesRowsIncludesId(previous, current.head.user.id)) {
+      /* eslint-disable camelcase */
+      userParams = Object.assign({}, current.head.user);
+      userParams.created_at = (userParams.created_at) ? new Date(userParams.created_at) : null;
+      userParams.updated_at = (userParams.updated_at) ? new Date(userParams.updated_at) : null;
+      /* eslint-eable camelcase */
+
+      previous.push(
+        usersTable.createRow(userParams)
+      );
+    }
+    // pull's assignee
+    if (current.assignee && !doesRowsIncludesId(previous, current.assignee.id)) {
+      /* eslint-disable camelcase */
+      userParams = Object.assign({}, current.assignee);
+      userParams.created_at = (userParams.created_at) ? new Date(userParams.created_at) : null;
+      userParams.updated_at = (userParams.updated_at) ? new Date(userParams.updated_at) : null;
+      /* eslint-eable camelcase */
+
+      previous.push(
+        usersTable.createRow(userParams)
+      );
+    }
+    // pull's user
+    if (current.user && !doesRowsIncludesId(previous, current.user.id)) {
+      /* eslint-disable camelcase */
+      userParams = Object.assign({}, current.user);
+      userParams.created_at = (userParams.created_at) ? new Date(userParams.created_at) : null;
+      userParams.updated_at = (userParams.updated_at) ? new Date(userParams.updated_at) : null;
+      /* eslint-eable camelcase */
+
+      previous.push(
+        usersTable.createRow(userParams)
+      );
+    }
+
+    // pull-base-repo's owner
+    if (current.base && current.base.repo && current.base.repo.owner && !doesRowsIncludesId(previous, current.base.repo.owner.id)) {
+      /* eslint-disable camelcase */
+      userParams = Object.assign({}, current.base.repo.owner);
+      userParams.created_at = (userParams.created_at) ? new Date(userParams.created_at) : null;
+      userParams.updated_at = (userParams.updated_at) ? new Date(userParams.updated_at) : null;
+      /* eslint-eable camelcase */
+
+      previous.push(
+        usersTable.createRow(userParams)
+      );
+    }
+
+    // pull-base's user
+    if (current.base && current.base.user && !doesRowsIncludesId(previous, current.base.user.id)) {
+      /* eslint-disable camelcase */
+      userParams = Object.assign({}, current.base.user);
+      userParams.created_at = (userParams.created_at) ? new Date(userParams.created_at) : null;
+      userParams.updated_at = (userParams.updated_at) ? new Date(userParams.updated_at) : null;
+      /* eslint-eable camelcase */
+
+      previous.push(
+        usersTable.createRow(userParams)
+      );
+    }
+
+    return previous;
+  }, []);
+
+  // insert_or_replace users
+  await db.insertOrReplace().into(usersTable).values(userRows).exec();
+
   return pulls;
 };
 
